0e4fb855d4fb0b48b33e73d93f704202bcf41afd
Jack Grahl <jack.grahl@gmail.com>
2017-04-22 21:38:13 +0000

    Add a LogicGrid class and some tests

---
+++ LogicPuzzle/LogicGrid.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogicPuzzle
+{
+    public class LogicGrid<T, U> : Dictionary<Tuple<T, U>, bool?>
+    {
+        public void SetFalse(T t, U u)
+        {
+            this[new Tuple<T, U>(t, u)] = false;
+        }
+
+        public bool? this[T t, U u]
+        {
+            get => this[new Tuple<T, U>(t, u)];
+            set => this[new Tuple<T, U>(t, u)] = value;
+        }
+    }
+}
---
+++ TestLogicPuzzle/TestLogicGrid.cs
@@ -0,0 +1,46 @@
+﻿using LogicPuzzle;
+using Xunit;
+
+namespace TestLogicPuzzle
+{
+    internal enum Fish
+    {
+        Haddock,
+        Plaice,
+        Cod,
+        Rock,
+        Flake
+    }
+
+    internal enum Currency
+    {
+        GBP,
+        EUR,
+        USD,
+        AUD,
+        KRW
+    }
+
+    public class TestLogicGrid
+    {
+        private LogicGrid<Fish, Currency> _grid;
+
+        [Fact]
+        public void TestSetOneFalse()
+        {
+            _grid = new LogicGrid<Fish, Currency>();
+            _grid[Fish.Haddock, Currency.EUR] = false;
+            Assert.False(_grid[Fish.Haddock, Currency.EUR]);
+        }
+
+        [Fact]
+        public void TestSetOneTrue()
+        {
+            _grid = new LogicGrid<Fish, Currency>();
+            _grid[Fish.Haddock, Currency.EUR] = true;
+            Assert.True(_grid[Fish.Haddock, Currency.EUR]);
+            Assert.False(_grid[Fish.Haddock, Currency.KRW]);
+            Assert.False(_grid[Fish.Cod, Currency.EUR]);
+        }
+    }
+}

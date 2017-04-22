4e32e06b92f83d573bd749c9db72cf530b80e8f4
Jack Grahl <jack.grahl@gmail.com>
2017-04-22 21:51:45 +0000

    Update the grid if we set a new value to true

--- LogicPuzzle/LogicGrid.cs
+++ LogicPuzzle/LogicGrid.cs
@@ -15,5 +15,25 @@ namespace LogicPuzzle
             get => this[new Tuple<T, U>(t, u)];
             set => this[new Tuple<T, U>(t, u)] = value;
         }
+
+        public new bool? this[Tuple<T, U> x]
+        {
+            get { return base[x]; }
+            set {
+                base[x] = value;
+                if (value == true){
+                    foreach (T t in Enum.GetValues(typeof(T))) {
+                        if (! t.Equals(x.Item1)) {
+                            this[t, x.Item2] = false;    
+                        }
+                    }
+                    foreach (U u in Enum.GetValues(typeof(U))) {
+                        if (! u.Equals(x.Item2)) {
+                            this[x.Item1, u] = false;    
+                        }
+                    }
+                }
+            }
+        }
     }
 }
